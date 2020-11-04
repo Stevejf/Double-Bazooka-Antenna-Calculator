af5909119a8841073f0d43664e03b0d58109a5cf
Stevejf <sfraser857d@gmail.com>
2020-11-04 21:25:09 +1100

    Update ant.py
    added half-length

--- ant.py
+++ ant.py
@@ -15,15 +15,15 @@ FORMULA_CENTRE = 299.792458/user*0.6595*0.50
 END = (FORMULA_TOTAL-FORMULA_CENTRE)/2
 FINAL_RESULT = f"""             your coaxial dipole length in metres
                     (frequency = {user} mHz)
-                             total
+                         total length
           |------------------{FORMULA_TOTAL:.3f}------------------|
           -----------==========-==========-----------
           
-                             centre
-                    |--------{FORMULA_CENTRE:.3f}--------|         
+                         braid length
+                    |--------{FORMULA_CENTRE:.3f}-------|         
           -----------==========-==========-----------
-          |--{END:.3f}--|
-          end length""".title()
+          |--{END:.3f}--|          |--------{FORMULA_TOTAL/2:.3f}--------|
+          end length                  half length""".title()
 print("\n"*50)
 write_to_txt_file = open("bazooka.txt", "w") 
 write_to_txt_file.write(FINAL_RESULT) 
